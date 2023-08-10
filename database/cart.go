db9b3eefbb53563df0bada9c55b81f20c427a9d3
IlnarAhm <ilnar.ahma@yandex.ru>
2023-08-10 16:12:07 +0300

    database config

---
+++ database/cart.go
@@ -0,0 +1,29 @@
+package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("Can't find the product")
+	ErrCantDecodeProducts = errors.New("Can't find the products")
+	ErrUserIdIsNotValid   = errors.New("This user is not valid")
+	ErrCantUpdateUser     = errors.New("Cannot add this product to the cart")
+	ErrCantRemoveItem     = errors.New("Cannot remove this item from the cart")
+	ErrCantGetItem        = errors.New("Was unnable to get the item from the cart")
+	ErrCantBuyItem        = errors.New("Cannot update the purchase")
+)
+
+func Add() {
+
+}
+
+func Remove() {
+
+}
+
+func Buy() {
+
+}
+
+func Buyer() {
+
+}
