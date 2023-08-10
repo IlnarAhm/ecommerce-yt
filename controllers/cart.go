38c2ac4c47ce01b7571d4cdac237cc46d79d7bda
Ilnar Ahmadiev <iiakhmadiev@1bit.com>
2023-08-10 12:02:59 +0300

    Add controllers

---
+++ controllers/cart.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddToCart() gin.HandlerFunc {
+
+}
+
+func RemoveItem() gin.HandlerFunc {
+
+}
+
+func GetItem() gin.HandlerFunc {
+
+}
+
+func Buy() gin.HandlerFunc {
+
+}
+
+func InstantBuy() gin.HandlerFunc {
+
+}
