e3fe27a3dbbec9150cf6057a8fb85fdfefdbe09c
Ilnar Ahmadiev <iiakhmadiev@1bit.com>
2023-08-10 11:08:49 +0300

    main.go, docker-compose, routes

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/IlnarAhm/ecommerce-yt/controllers"
+	"github.com/IlnarAhm/ecommerce-yt/database"
+	"github.com/IlnarAhm/ecommerce-yt/routes"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(
+		database.ProductData(database.client, "Products"),
+		database.UserData(database.client, "Users"),
+	)
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
 }
